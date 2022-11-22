6acc580c0fcfebe7d78c7763b0a8a339d3c6925b
mis306lu <msuranofsky@ebsco.com>
2022-11-22 14:49:01 -0500

    formatting query

--- src/main/java/org/folio/ncip/FolioRemoteServiceManager.java
+++ src/main/java/org/folio/ncip/FolioRemoteServiceManager.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -738,12 +739,17 @@ public class FolioRemoteServiceManager implements RemoteServiceManager {
 	 *
 	 */
 	public JsonObject lookupPatronRecord(UserId userid) throws Exception {
-		//String barcode = userid.getUserIdentifierValue();
 		String userIdentifier = userid.getUserIdentifierValue();
-		// LOOKUP THE PATRON
 		String baseUrl = okapiHeaders.get(Constants.X_OKAPI_URL);
-		//String userApiUri = baseUrl + "/users?query=(barcode==" + barcode + ")&limit=1";
-		String userApiUri = baseUrl + "/users?query=(barcode==" + userIdentifier + "%20or%20username==" + userIdentifier + "%20or%20externalSystemId==" + userIdentifier + ")&limit=1";
+		StringBuilder query = new StringBuilder()
+		          .append("(barcode==")
+		          .append(userIdentifier)
+		          .append(" or externalSystemId==")
+		          .append(userIdentifier)
+		          .append(" or username==")
+		          .append(userIdentifier)
+		          .append(')');
+		String userApiUri = baseUrl + "/users?query="  + URLEncoder.encode(query.toString(),StandardCharsets.UTF_8);
 		String response = callApiGet(userApiUri);
 
 		// WAS THE PATRON FOUND?
