b4b01ed4def93510a8b6a94656b27a1cf5fd7edc
mis306lu <msuranofsky@ebsco.com>
2023-04-05 12:07:20 -0400

    check user type

--- src/main/java/org/folio/ncip/FolioRemoteServiceManager.java
+++ src/main/java/org/folio/ncip/FolioRemoteServiceManager.java
@@ -781,7 +781,8 @@ public class FolioRemoteServiceManager implements RemoteServiceManager {
 		if (!validTypes.contains(type)) {
 			throw new Exception("invalid patron lookup type provided: " + type);
 		}
-		if (type != null && value.equalsIgnoreCase("externalSystemId")) type = "externalSystemId";
+		logger.info
+		if (type != null && type.equalsIgnoreCase("externalSystemId")) type = "externalSystemId";
 		value = StringUtil.cqlEncode(value);
 		String baseUrl = okapiHeaders.get(Constants.X_OKAPI_URL);
 		String query = "(" + type + "==" + value + ")";
