dd1f16aa22d1e2a76ee0f99bd0467541c32121fd
mis306lu <msuranofsky@ebsco.com>
2023-09-13 16:33:21 -0400

    Update RequestItem.java

--- src/test/java/org/folio/ncip/RequestItem.java
+++ src/test/java/org/folio/ncip/RequestItem.java
@@ -30,8 +30,6 @@ import com.jayway.restassured.path.xml.XmlPath.CompatibilityMode;
  */
 public class RequestItem extends TestBase {
 	
-
-
 	/**
 	 * @throws java.lang.Exception
 	 */
@@ -40,38 +38,9 @@ public class RequestItem extends TestBase {
 	}
 
 	@Test
-	public void callLookupUserSuccess() throws MalformedURLException {
-			Response response = postData("src/test/resources/mockdata/ncip-lookupUser.xml");
-			response.then().assertThat().body("NCIPMessage.LookupUserResponse.UserId.UserIdentifierValue", Matchers.equalTo(TestConstants.GOOD_PATRON_BARCODE));
-			response.then().assertThat().body("NCIPMessage.LookupUserResponse.UserOptionalFields.UserPrivilege[2].UserPrivilegeStatus.UserPrivilegeStatusType", Matchers.equalTo(TestConstants.ACTIVE));
-			response.then().assertThat().body("NCIPMessage.LookupUserResponse.UserOptionalFields.UserPrivilege[0].UserPrivilegeStatus.UserPrivilegeStatusType", Matchers.equalTo("GRAD"));
-			System.out.println(response.getBody().prettyPrint());
-	}
-	
-	@Test
-	public void callLookupUserNotFound() throws MalformedURLException {
-		Response response = postData("src/test/resources/mockdata/ncip-lookupUserNotFound.xml");
-		response.then().assertThat().body("NCIPMessage.LookupUserResponse.Problem", Matchers.not(Matchers.isEmptyOrNullString()));
-		response.then().assertThat().body("NCIPMessage.LookupUserResponse.Problem.ProblemValue", Matchers.containsString(TestConstants.DID_NOT_FIND));
-		System.out.println(response.getBody().prettyPrint());
-	}
-	
-	@Test
-	public void callLookupUserBlocked() throws MalformedURLException {
-		Response response = postData("src/test/resources/mockdata/ncip-lookupUserBlocked.xml");
-		response.then().assertThat().body("NCIPMessage.LookupUserResponse.UserId.UserIdentifierValue", Matchers.equalTo(TestConstants.BLOCKED_PATRON_BARCODE));
-		response.then().assertThat().body("NCIPMessage.LookupUserResponse.UserOptionalFields.UserPrivilege[2].UserPrivilegeStatus.UserPrivilegeStatusType", Matchers.equalTo(TestConstants.BLOCKED));
-		System.out.println(response.getBody().prettyPrint());
-	}
-	
-	
-	@Test
 	public void callRequestItem() throws MalformedURLException {
 		Response response = postData("src/test/resources/mockdata/ncip-requestitem.xml");
 		System.out.println(response.getBody().prettyPrint());
 	}
-
-
-
 
 }
