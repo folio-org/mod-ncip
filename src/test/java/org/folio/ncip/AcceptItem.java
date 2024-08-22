784b71576ae5da2ac92f380f501fe95d5bdec18d
Janis Saldabols <janis@indexdata.com>
2024-08-22 14:57:17 +0300

    PR-1908 Add item uuid to AcceptItem

--- src/test/java/org/folio/ncip/AcceptItem.java
+++ src/test/java/org/folio/ncip/AcceptItem.java
@@ -11,12 +11,14 @@ import static org.junit.Assert.assertTrue;
 public class AcceptItem extends TestBase {
 
     private static final String REQUEST_ID = "de627598-d468-41e0-a849-450940478477";
+    private static final String ITEM_UUID = "7212ba6a-8dcf-45a1-be9a-ffaa847c4423";
     @Test
     public void acceptItem() throws MalformedURLException {
         Response response = postData("src/test/resources/mockdata/ncip-acceptItem.xml");
         String body = response.getBody().prettyPrint();
         assertEquals(200, response.getStatusCode());
         assertTrue(body.contains(REQUEST_ID));
+        assertTrue(body.contains(ITEM_UUID));
     }
 
     @Test
